2524153de21d637935171eb8a3f353bf906e7521
Julia Saari <mariajulia.saari@gmail.com>
2020-11-17 15:21:27 +0200

    controller ok

--- index.js
+++ index.js
@@ -3,11 +3,17 @@ const express = require('express')
 
 const app = express()
 const cors = require('cors')
-const Blog = require('./models/blog')
-
+const blogsRouter = require('./controllers/blogs')
 const logger = require('./utils/logger')
 const config = require('./utils/config')
+
+
+
+
 const mongoose = require('mongoose')
+
+
+
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
         .then(() => {
@@ -19,25 +25,7 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
 
 app.use(cors())
 app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog 
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
+app.use('/api/blogs', blogsRouter)
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`);
