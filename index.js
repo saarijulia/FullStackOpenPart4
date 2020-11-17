b0a41148e5dabf2b5f57c05a0bf5f53bd5906bec
Julia Saari <mariajulia.saari@gmail.com>
2020-11-17 15:32:44 +0200

    refactor done

--- index.js
+++ index.js
@@ -1,32 +1,10 @@
+const app = require('./app')
 const http = require('http')
-const express = require('express')
-
-const app = express()
-const cors = require('cors')
-const blogsRouter = require('./controllers/blogs')
-const logger = require('./utils/logger')
 const config = require('./utils/config')
+const logger = require('./utils/logger')
 
+const server = http.createServer(app)
 
-
-
-const mongoose = require('mongoose')
-
-
-
-
-mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-        .then(() => {
-          logger.info('connected to MongoDB')
-        })
-        .catch((error) => {
-          logger.error('error connecting to MongoDB: ', error.message)
-        })
-
-app.use(cors())
-app.use(express.json())
-app.use('/api/blogs', blogsRouter)
-
-app.listen(config.PORT, () => {
+server.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`);
 })
