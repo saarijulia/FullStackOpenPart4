f61ac57fbe31a77fbba03bdc7e7a1cc5be95586a
Julia Saari <mariajulia.saari@gmail.com>
2020-12-28 10:05:37 +0200

    4.8 ok

---
+++ tests/bloglist_api.test.js
@@ -0,0 +1,69 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const blogsRouter = require('../controllers/blogs')
+
+const api = supertest(app)
+const Blog = require('../models/blog')
+const initialBlogs = [
+    {
+        title: 'Outfits 101',
+        author: 'Brandon D',
+        url: 'outfits1O1.com',
+        likes: 500
+    },
+
+    {
+        title: 'The art of baking',
+        author: 'Lily Collins',
+        url: 'theartofbaking.com',
+        likes: 67
+    }
+]
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+    let blogObject = new Blog(initialBlogs[0])
+    await blogObject.save()
+    blogObject = new Blog(initialBlogs[1])
+    await blogObject.save()
+    console.log('initial blogs:', initialBlogs)
+  })
+
+  test('blogs can be retrieved', async () => {
+      await api
+        .get('/api/blogs/')
+        .expect('Content-Type', /application\/json/)
+
+    const response = await api.get('/api/blogs')
+    console.log('contents', response.body)
+    expect(response.body.length).toEqual(initialBlogs.length)
+    
+  })
+
+/*test('a valid blog can be added', async () => {
+   const newBlog = {
+       title: 'Api Test blog',
+       author: 'Julia',
+       url: 'test.com',
+       likes: 1
+   }
+
+   await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+    const response = await api.get('/api/blogs')
+    const contents = response.body.map(r=> r.content)
+    console.log('test array contents:', contents)
+    expect (response.body).toHaveLength(initialBlogs.length + 1)
+    expect(contents).toContain(
+        'Api Test blog'
+    )
+})*/
+
+afterAll(() => {
+    mongoose.connection.close()
+  }) 
