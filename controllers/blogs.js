858d0deaf1e750133a8d4696975489633ef550f8
Julia Saari <mariajulia.saari@gmail.com>
2020-12-28 12:51:40 +0200

    4.13 ok

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -18,12 +18,39 @@ blogsRouter.post('/', (request, response) => {
     if (!blog.title || !blog.author){
         response.status(400).send({error: 'missing content'})
     }
-    
+
     blog
         .save()
         .then(result => {
             response.status(201).json(result)
         })
 })
+
+blogsRouter.get('/:id', (request, response, next) => {
+    const main = async () => {
+        const blog = await Blog.findById(request.params.id)
+        console.log('operation returned the following blog:', blog)
+        if (blog) {
+            response.json(blog)
+        } else {
+            response.status(404)
+        }
+    }
+    main()
+})
+
+blogsRouter.delete('/:id', (request, response, next) => {
+    Blog.findByIdAndRemove(request.params.id)
+        .then(blog => {
+            if (blog) {
+                console.log('deleted', blog)
+
+            } else {
+                response.status(204).end()
+            }
+        })
+        .catch(error => next(error))
+})
+
 
 module.exports = blogsRouter
