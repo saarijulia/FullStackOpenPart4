31f3866baac7ec83e9a8176a058b08dd96100e27
Julia Saari <mariajulia.saari@gmail.com>
2020-12-29 16:51:24 +0200

    4.19 ok

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const blogsRouter = require('./controllers/blogs')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 logger.info('connecting to', config.MONGODB_URI)
 
@@ -21,5 +22,6 @@ app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 module.exports = app
