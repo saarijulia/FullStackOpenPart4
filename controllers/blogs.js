47540871588460eae2e3d6e4c8d506ee83c2ce12
Julia Saari <mariajulia.saari@gmail.com>
2020-12-29 15:38:38 +0200

    4.17 ok

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,30 +1,37 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
-blogsRouter.get('/', (request, response) => {
-    Blog
+blogsRouter.get('/', async(request, response) => {
+    const blogs = await Blog
         .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
+        .populate('user', { username: 1, name: 1})
+        
+    response.json(blogs)
 })
 
-blogsRouter.post('/', (request, response) => {
-    const blog = new Blog(request.body)
+blogsRouter.post('/', async(request, response) => {
+    const body = request.body
+    const user =  await User.findById(body.userId)
+
+    const blog = new Blog({
+        author: body.author,
+        title: body.title,
+        likes: body.likes,
+        url: body.url,
+        user: user._id
+    })
     if (!blog.likes) {
         blog.likes = 0;
-    }
-
+    } 
     if (!blog.title || !blog.author){
         response.status(400).send({error: 'missing content'})
-    } else {
-        blog
-        .save()
-        .then(result => {
-            response.status(201).json(result)
-        })
-    }
-
+    } 
+        const savedBlog = await blog.save()
+        user.blogs = user.blogs.concat(savedBlog._id)
+        await user.save()
+        response.json(savedBlog)
+    
    
 })
 
